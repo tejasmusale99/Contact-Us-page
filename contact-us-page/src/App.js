521ad962f2207981a75c143cb6557d1f297c9a1f
Tejas Musale <tejasmusale99@gmail.com>
2023-07-07 19:28:44 +0530

    Conact header done

--- contact-us-page/src/App.js
+++ contact-us-page/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
+import ContactHeader from './Components/ContactHeader/ContactHeader';
 
 
 function App() {
   return (
    <div>
     <Navigation />
+    <ContactHeader />
    </div>
   );
 }
